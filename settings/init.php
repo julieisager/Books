3e7e68a782d2e7d0b19b22d133c9fae1dcc715f6
Julie <julie@isager.dk>
2023-10-12 16:42:31 +0200

    opgave 3

--- settings/init.php
+++ settings/init.php
@@ -1,7 +1,7 @@
 <?php
 require "classes/classDB.php";
 
-define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
+define("CONFIG_LIVE", "1"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
     $DB_SERVER = "localhost";
@@ -9,10 +9,10 @@ if(CONFIG_LIVE == 0){
     $DB_USER = "root";
     $DB_PASS = "";
 }else{
-    $DB_SERVER = "";
-    $DB_NAME = "";
-    $DB_USER = "";
-    $DB_PASS = "";
+    $DB_SERVER = "mysql97.unoeuro.com";
+    $DB_NAME = "julieisager_dk_db";
+    $DB_USER = "julieisager_dk";
+    $DB_PASS = "gG4tFrmwx5HhAab3pz6B";
 }
 
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
